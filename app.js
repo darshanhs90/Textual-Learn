24ad637e03b3693d25e16692162ca790efe0efb7
Haridarshan HS <haridarshan.h.s@ericsson.com>
2015-10-30 20:29:35 -0500

    package changed

--- app.js
+++ app.js
@@ -1,18 +1,66 @@
-var querystring = require('querystring');
+var app = require('express')();
+var express=require('express');
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var ahttp = require('http');
+//if (process.env.NODE_ENV !== 'production'){
+var longjohn= require('longjohn');
+//}
+longjohn.async_trace_limit = -1;  // unlimited
 var request = require('request');
-var express = require('express');
-var xml2js = require('xml2js');
-var parser = new xml2js.Parser();
 var https = require('https');
 var cors = require('cors');
-var lang = 'spa';
-var getMsg = false;
-var textMain = '';
+var Twitter = require('twitter');
+var client = new Twitter({
+    consumer_key: 'LmNp3JwAQZnuBr4SQFaM7UZG3',
+    consumer_secret: 'Xps6ziqIhZ0exAPoIAeyqj7myu7L78ZLHQDni67dzD9koJQTAD',
+    access_token_key: '151128859-F4Wk8KebqH4ZDwp8tMWY8PkoTQzfiEJrN1t2Knfc',
+    access_token_secret: 'czQre16YZKoC4Csi18gGufu8PxF733aL5VnzbhurlGvHw'
+});
+var Twitter1 = require('node-tweet-stream'),
+    tw = new Twitter1({
+        consumer_key: 'LmNp3JwAQZnuBr4SQFaM7UZG3',
+        consumer_secret: 'Xps6ziqIhZ0exAPoIAeyqj7myu7L78ZLHQDni67dzD9koJQTAD',
+        token: '151128859-F4Wk8KebqH4ZDwp8tMWY8PkoTQzfiEJrN1t2Knfc',
+        token_secret: 'czQre16YZKoC4Csi18gGufu8PxF733aL5VnzbhurlGvHw'
+    });
+var watson = require('watson-developer-cloud');
+var AlchemyAPI = require('alchemy-api');
+var alchemy = new AlchemyAPI('0554d03cab53ef907d02d27eaea5c2938b471ef1');
+var sendgrid = require('sendgrid')('hsdars', 'Password90-');
+var accountSid = 'AC07275e4294f1b0d42623c3ec9559911e';
+var authToken = '650d049a9bd99323fb899ce4b9e84fcc';
+var clientTwilio = require('twilio')(accountSid, authToken);
+var Twit = require('twit');
+var sanFrancisco = ['-122.75', '36.8', '-121.75', '37.8']
+var T = new Twit({
+    consumer_key: 'LmNp3JwAQZnuBr4SQFaM7UZG3',
+    consumer_secret: 'Xps6ziqIhZ0exAPoIAeyqj7myu7L78ZLHQDni67dzD9koJQTAD',
+    access_token: '151128859-F4Wk8KebqH4ZDwp8tMWY8PkoTQzfiEJrN1t2Knfc',
+    access_token_secret: 'czQre16YZKoC4Csi18gGufu8PxF733aL5VnzbhurlGvHw'
+})
 
+var OAuth = require('oauth').OAuth,
+    oauth = new OAuth(
+        "https://api.twitter.com/oauth/request_token",
+        "https://api.twitter.com/oauth/access_token",
+        "LmNp3JwAQZnuBr4SQFaM7UZG3",
+        "Xps6ziqIhZ0exAPoIAeyqj7myu7L78ZLHQDni67dzD9koJQTAD",
+        "1.0",
+        "oob",
+        "HMAC-SHA1"
+    );
+var xoauth;
+
+var Bing = require('node-bing-api')({
+    accKey: "l11l8D4FBj6XkyHh3NzeMINbdY+s19eUoxrRgvgQQgQ"
+});
+
+// cfenv provides access to your Cloud Foundry environment
+// for more info, see: https://www.npmjs.com/package/cfenv
 var cfenv = require('cfenv');
 
 // create a new express server
-var app = express();
 app.use(cors());
 // serve the files out of ./public as our main files
 app.use(express.static(__dirname + '/public'));
@@ -22,12 +70,32 @@ app.use(express.static(__dirname + '/public'));
 var appEnv = cfenv.getAppEnv();
 
 // start server on the specified port and binding host
-app.listen(appEnv.port, appEnv.bind, function() {
-    //app.listen(1337, '127.0.0.1', function() {
+server.listen(appEnv.port, appEnv.bind, function() {
+//server.listen(1337, '127.0.0.1', function() {
 
     // print a message when the server starts listening
     console.log("server starting on " + appEnv.url);
 });
+
+
+
+
+
+
+
+var querystring = require('querystring');
+var xml2js = require('xml2js');
+var parser = new xml2js.Parser();
+
+
+
+
+var lang = 'spa';
+var getMsg = false;
+var textMain = '';
+
+
+
 var verify = false,subscribe=false;
 var number;
 
