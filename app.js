62f7c8fd392996c60cf578b23ff49e9066d3feb7
Haridarshan HS <haridarshan.h.s@ericsson.com>
2015-11-03 16:33:32 -0600

    server modified

--- app.js
+++ app.js
@@ -94,6 +94,7 @@ app.get("/subscribe", function(e, t) {
     lang = e.query.lang, getMsg = 1 == e.query.method ? !0 : !1, console.log(getMsg), subscribe = !0, console.log("subscribed"), t.end()
 });
 setInterval(function() {
+    console.log(number);
     console.log("verify is" + verify), console.log("subscribe is " + subscribe), 1 == verify && 1 == subscribe && request("http://randomword.setgetgo.com/get.php", function(e, t, r) {
         textMain = "", textMain += "Word :" + r;
         var n = r;
