ec099f7d00b7d62abd96e096cdd0e39925b28f35
Haridarshan HS <haridarshan.h.s@ericsson.com>
2015-11-03 19:27:06 -0600

    server modified

--- app.js
+++ app.js
@@ -1,66 +1,70 @@
 var app = require("express")(),
-    express = require("express"),
-    server = require("http").Server(app),
-    io = require("socket.io")(server),
-    ahttp = require("http"),
-    longjohn = require("longjohn");
+express = require("express"),
+server = require("http").Server(app),
+io = require("socket.io")(server),
+ahttp = require("http"),
+longjohn = require("longjohn");
 var bodyParser=require('body-parser')
 longjohn.async_trace_limit = -1;
 var request = require("request"),
-    https = require("https"),
-    cors = require("cors"),
-    Twitter = require("twitter"),
-    client = new Twitter({
-        consumer_key: "LmNp3JwAQZnuBr4SQFaM7UZG3",
-        consumer_secret: "Xps6ziqIhZ0exAPoIAeyqj7myu7L78ZLHQDni67dzD9koJQTAD",
-        access_token_key: "151128859-F4Wk8KebqH4ZDwp8tMWY8PkoTQzfiEJrN1t2Knfc",
-        access_token_secret: "czQre16YZKoC4Csi18gGufu8PxF733aL5VnzbhurlGvHw"
-    }),
-    Twitter1 = require("node-tweet-stream"),
-    tw = new Twitter1({
-        consumer_key: "LmNp3JwAQZnuBr4SQFaM7UZG3",
-        consumer_secret: "Xps6ziqIhZ0exAPoIAeyqj7myu7L78ZLHQDni67dzD9koJQTAD",
-        token: "151128859-F4Wk8KebqH4ZDwp8tMWY8PkoTQzfiEJrN1t2Knfc",
-        token_secret: "czQre16YZKoC4Csi18gGufu8PxF733aL5VnzbhurlGvHw"
-    }),
-    watson = require("watson-developer-cloud"),
-    AlchemyAPI = require("alchemy-api"),
-    alchemy = new AlchemyAPI("0554d03cab53ef907d02d27eaea5c2938b471ef1"),
-    sendgrid = require("sendgrid")("hsdars", "Password90-"),
-    accountSid = "AC07275e4294f1b0d42623c3ec9559911e",
-    authToken = "650d049a9bd99323fb899ce4b9e84fcc",
-    clientTwilio = require("twilio")(accountSid, authToken),
-    Twit = require("twit"),
-    sanFrancisco = ["-122.75", "36.8", "-121.75", "37.8"],
-    T = new Twit({
-        consumer_key: "LmNp3JwAQZnuBr4SQFaM7UZG3",
-        consumer_secret: "Xps6ziqIhZ0exAPoIAeyqj7myu7L78ZLHQDni67dzD9koJQTAD",
-        access_token: "151128859-F4Wk8KebqH4ZDwp8tMWY8PkoTQzfiEJrN1t2Knfc",
-        access_token_secret: "czQre16YZKoC4Csi18gGufu8PxF733aL5VnzbhurlGvHw"
-    }),
-    OAuth = require("oauth").OAuth,
-    oauth = new OAuth("https://api.twitter.com/oauth/request_token", "https://api.twitter.com/oauth/access_token", "LmNp3JwAQZnuBr4SQFaM7UZG3", "Xps6ziqIhZ0exAPoIAeyqj7myu7L78ZLHQDni67dzD9koJQTAD", "1.0", "oob", "HMAC-SHA1"),
-    xoauth, Bing = require("node-bing-api")({
-        accKey: "l11l8D4FBj6XkyHh3NzeMINbdY+s19eUoxrRgvgQQgQ"
-    }),
-    cfenv = require("cfenv");
-    app.use(bodyParser.urlencoded({ extended: true })); 
+https = require("https"),
+cors = require("cors"),
+Twitter = require("twitter"),
+client = new Twitter({
+    consumer_key: "LmNp3JwAQZnuBr4SQFaM7UZG3",
+    consumer_secret: "Xps6ziqIhZ0exAPoIAeyqj7myu7L78ZLHQDni67dzD9koJQTAD",
+    access_token_key: "151128859-F4Wk8KebqH4ZDwp8tMWY8PkoTQzfiEJrN1t2Knfc",
+    access_token_secret: "czQre16YZKoC4Csi18gGufu8PxF733aL5VnzbhurlGvHw"
+}),
+Twitter1 = require("node-tweet-stream"),
+tw = new Twitter1({
+    consumer_key: "LmNp3JwAQZnuBr4SQFaM7UZG3",
+    consumer_secret: "Xps6ziqIhZ0exAPoIAeyqj7myu7L78ZLHQDni67dzD9koJQTAD",
+    token: "151128859-F4Wk8KebqH4ZDwp8tMWY8PkoTQzfiEJrN1t2Knfc",
+    token_secret: "czQre16YZKoC4Csi18gGufu8PxF733aL5VnzbhurlGvHw"
+}),
+watson = require("watson-developer-cloud"),
+AlchemyAPI = require("alchemy-api"),
+alchemy = new AlchemyAPI("0554d03cab53ef907d02d27eaea5c2938b471ef1"),
+sendgrid = require("sendgrid")("hsdars", "Password90-"),
+accountSid = "AC07275e4294f1b0d42623c3ec9559911e",
+authToken = "650d049a9bd99323fb899ce4b9e84fcc",
+clientTwilio = require("twilio")(accountSid, authToken),
+Twit = require("twit"),
+sanFrancisco = ["-122.75", "36.8", "-121.75", "37.8"],
+T = new Twit({
+    consumer_key: "LmNp3JwAQZnuBr4SQFaM7UZG3",
+    consumer_secret: "Xps6ziqIhZ0exAPoIAeyqj7myu7L78ZLHQDni67dzD9koJQTAD",
+    access_token: "151128859-F4Wk8KebqH4ZDwp8tMWY8PkoTQzfiEJrN1t2Knfc",
+    access_token_secret: "czQre16YZKoC4Csi18gGufu8PxF733aL5VnzbhurlGvHw"
+}),
+OAuth = require("oauth").OAuth,
+oauth = new OAuth("https://api.twitter.com/oauth/request_token", "https://api.twitter.com/oauth/access_token", "LmNp3JwAQZnuBr4SQFaM7UZG3", "Xps6ziqIhZ0exAPoIAeyqj7myu7L78ZLHQDni67dzD9koJQTAD", "1.0", "oob", "HMAC-SHA1"),
+xoauth, Bing = require("node-bing-api")({
+    accKey: "l11l8D4FBj6XkyHh3NzeMINbdY+s19eUoxrRgvgQQgQ"
+}),
+cfenv = require("cfenv");
+app.use(bodyParser.urlencoded({ extended: true })); 
 app.use(cors()), app.use(express["static"](__dirname + "/public"));
 var appEnv = cfenv.getAppEnv();
 server.listen(appEnv.port, appEnv.bind, function() {
     console.log("server starting on " + appEnv.url)
 });
 var querystring = require("querystring"),
-    xml2js = require("xml2js"),
-    parser = new xml2js.Parser,
-    lang = "spa",
-    getMsg = !1,
-    textMain = "",
-    verify = !1,
-    subscribe = !1,
-    number;
+xml2js = require("xml2js"),
+parser = new xml2js.Parser,
+lang = "spa",
+getMsg = !1,
+textMain = "",
+verify = !1,
+subscribe = !1;
+
+var number='1';
 app.get("/verifycode", function(e, t) {
-    number = e.query.number, console.log(number), https.get("https://api.nexmo.com/verify/json?api_key=638c2b46&api_secret=60539549&number=" + number + "&brand=Textual Learn", function(e) {
+    number = e.query.number;
+    console.log(' changing number');
+    console.log(number);
+    https.get("https://api.nexmo.com/verify/json?api_key=638c2b46&api_secret=60539549&number=" + number + "&brand=Textual Learn", function(e) {
         var r = "";
         e.on("data", function(e) {
             r += e
@@ -73,7 +77,7 @@ app.get("/verifycode", function(e, t) {
 });
 app.get("/verifycheck", function(e, t) {
     var r = e.query.code,
-        n = e.query.request_id;
+    n = e.query.request_id;
     console.log(r), console.log(n), https.get("https://api.nexmo.com/verify/check/json?api_key=638c2b46&api_secret=60539549&request_id=" + n + "&code=" + r, function(e) {
         var r = "";
         e.on("data", function(e) {
@@ -134,10 +138,10 @@ setInterval(function() {
                             })
                         })
                     })
-                }
-            }
-        })
-    })
+}
+}
+})
+})
 }, 15e3);
 
 
@@ -147,15 +151,15 @@ setInterval(function() {
 
 app.get('/newsms',function(req,res){
 
-res.writeHead(200, {'Content-Type': 'application/json'});
-res.end();
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.end();
 
 
 });
 app.post('/newsms',function(req,res){
 
-res.writeHead(200, {'Content-Type': 'application/json'});
-res.end();
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.end();
 
 
 });
